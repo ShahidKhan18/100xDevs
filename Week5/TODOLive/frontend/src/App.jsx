17f72f057e1f652761ded8c2f0b0250f1a472f6b
Shahid Khan <shahidseran786@gmail.com>
2024-05-16 00:38:26 +0530

    Fronted Setup

---
+++ Week5/TODOLive/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import CreateTodo from './Components/CreateTodo'
+
+
+function App() {
+  
+
+  return (
+  <>
+    <div>
+      Hi There
+      <CreateTodo/>
+    </div>
+  </>
+  )
+}
+
+export default App
