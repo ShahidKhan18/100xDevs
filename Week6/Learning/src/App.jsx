1e54f30ce63efe291fcbe4fda21b02c2bbd7b409
Shahid Khan <shahidseran786@gmail.com>
2024-05-17 02:37:38 +0530

    Avoid Re-rendering using React.memo()

--- Week6/Learning/src/App.jsx
+++ Week6/Learning/src/App.jsx
@@ -1,31 +1,49 @@
-import { useState } from 'react'
+/* eslint-disable react/prop-types */
+import { memo, useState } from 'react'
 import './App.css'
-import Header from './Componets/Header'
+
 
 function App() {
+  const [randomNumber, setRandomNumber] = useState(0.999);
+  const titleHandler = () => {
+    setRandomNumber(Math.floor(Math.random() * 100));
+  };
    
   return (
-    <>
-      <HeaderWithButtons/>
+    <div>
+      <button onClick={titleHandler}>Click Me to Change to Title</button>
       <h3>My name is Shahid</h3>
-      <Header title={"Week 6 "} />
+      <Header title={"Week 6 "} ></Header>
       <Header title={"2nd Header "} />
-     
-    </>
+      <Header title={"2nd Header "} />
+      <Header title={"2nd Header "} />
+      <Header title={"2nd Header "} />
+      <Header title={"2nd Header "} />
+      <Header title={"2nd Header "} />
+      <Header title={randomNumber} />
+    </div>
   );
 }
 
-const HeaderWithButtons=()=>{
-   const [randomNumber, setRandomNumber] = useState(0.999);
-   const titleHandler = () => {
-     setRandomNumber(Math.floor(Math.random()*100));
-   };
-   return(
-    <>
-     <button onClick={titleHandler}>Click Me to Change to Title</button>
-      <h3>My name is {randomNumber}</h3>
-    </>
-   )
-}
+
+// - 1st way to avoid Multiple Re-rendering
+// const HeaderWithButtons=()=>{
+//    const [randomNumber, setRandomNumber] = useState(0.999);
+//    const titleHandler = () => {
+//      setRandomNumber(Math.floor(Math.random()*100));
+//    };
+//    return(
+//     <>
+//      <button onClick={titleHandler}>Click Me to Change to Title</button>
+//       <h3>My name is {randomNumber}</h3>
+//     </>
+//    )
+// }
+
+// -2nd way to avoid multiple re-rendering (using memo)
+
+const Header = memo(function Header({ title }) {
+  return <div>{title}</div>;
+});
 
 export default App
