332de20283d3e9232c59ba572d41ce7b70b2ab5b
Shahid Khan <shahidseran786@gmail.com>
2024-09-22 02:07:27 +0530

    Account DB Initialization

--- Week8/paytm-main/backend/DB/db.js
+++ Week8/paytm-main/backend/DB/db.js
@@ -34,8 +34,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+const accountSchema=mongoose.Schema({
+  userId:{
+   type:mongoose.Schema.Types.ObjectId,
+   ref:"User",
+   required:true,
+  },
+  ballance:{
+    type:Number,
+    required:true,
+  }
+})
+
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
   User,
+  Account,
 };
