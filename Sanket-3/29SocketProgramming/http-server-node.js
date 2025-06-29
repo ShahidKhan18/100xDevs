c629ae101f9c98056fb23ed1fc3ca54af067b8aa
Shahid Khan <shahidseran786@gmail.com>
2025-06-29 21:57:50 +0530

    ADD : Socket Programming

--- Sanket-3/29SocketProgramming/http-server-node.js
+++ Sanket-3/29SocketProgramming/http-server-node.js
@@ -1,10 +1,15 @@
+process.loadEnvFile();
+
+console.log("Hello ", process.env.name, " !")
+
 const http=require("http");
 const server=http.createServer((req,res)=>{
    console.log("New Connection was Created")
    if(req.url==="/home") return res.end("Home Page")
    res.end("Somthing somthing !")
-})
 
-server.listen(3000,()=>{
-    console.log("Server is listening on PORT : ",3000)
+})
+const port=process.env.PORT
+server.listen(port,()=>{
+    console.log("Server is listening on PORT : ",port)
 })
